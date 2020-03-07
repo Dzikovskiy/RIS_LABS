41c46389034ea8d9952c4e24972fc335019540b5
Vitali <vital.dz@mail.ru>
2020-03-07 16:29:32 +0300

    Initial commit

---
+++ Lab3/src/main/java/Test.java
@@ -0,0 +1,24 @@
+import entites.Client;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Test {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("PersistenceUnit",
+                System.getProperties());
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        Client client = new Client();
+        client.setUser_name("Jhonny2");
+        client.setFirst_address("Mins22k");
+        client.setSecond_address("Uruc2ha");
+
+        entityManager.persist(client);
+        entityManager.getTransaction().commit();
+
+        System.out.println("Id = "+ client.getIdClient());
+
+    }
+}
